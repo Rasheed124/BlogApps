0410f4a6caf173292ec4a9eda9d11e68c021c9f3
Rasheed124 <rashdevatrealglobe@gmail.com>
2024-08-16 19:36:39 +0100

    ZenBlog deployed successfully

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false, // Disable source maps to speed up the build process
+    minify: 'esbuild', // Use esbuild for faster minification
+  },
   server: {
     port: 5173,
     proxy: {
